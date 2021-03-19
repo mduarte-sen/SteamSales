b06f2601102e9dff3ef8e0394ba1342c5931163c
OkamiSen <maxium97@gmail.com>
2021-03-19 18:09:40 -0300

    implemented pagination button numbers for sales list

--- init.js
+++ init.js
@@ -1,7 +1,25 @@
 const DEAL_REDIRECT = "https://www.cheapshark.com/redirect?dealID="; //attach deal ID
 const STEAM_DEALS = "https://www.cheapshark.com/api/1.0/deals?storeID=1";
+const PAGE_NUMBER = "&pageNumber=";
+const PAGE_SIZE = "&pageSize=60";
+const SORT_BY = "&sortBy="; //possible values: Deal Rating,Title, Savings, Price, Metacritic, Reviews, Release, Store, recent
+const DESC = "&desc";
+const LOWER_PRICE = "&lowerPrice=";
+const UPPER_PRICE = "&upperPrice=";
+const METACRITIC = "&metacritic=";
+const STEAM_RATING = "&steamRating=";
+const STEAMAPP_ID = "&steamAppID=";
+const TITLE = "&title=";
+const EXACT = "&exact="; //Flag to allow only exact string match for "title" parameter
+const AAA = "&AAA="; //Flag to include only deals with retail price > $29
+const ON_SALE = "&onSale=";
+
 
 let salesArray = [];
+let displayPageNo = 0;
+let contentToAppend = ``;
+let paginationToAppend = ``;
+
 
 var getJSONData = function(url){
     var result = {};
@@ -22,38 +40,57 @@ var getJSONData = function(url){
     });
 }
 
+function appendPagination(){
+    for(let i=0; i<salesArray.length; i++){
+        paginationToAppend += `<li class="page-item">
+                                    <a class="page-link page-selection" href="#" id="select-page${i}">${i+1}</a>
+                                </li>`;
+        $(document).on("click", `#select-page${i}`, function(){
+            displayPageNo = $(`#select-page${i}`).text();
+            displayPageNo -= 1;
+            appendSalesList();
+            console.log(displayPageNo);
+        }); 
+    };
+    $('#previous-item').after(paginationToAppend);
+}
 
-
-
-$(document).ready(function(){
-let contentToAppend = ``;
-    getJSONData(STEAM_DEALS + '&onSale&pageSize=10').then(
-        function(resultObj){
-            if(resultObj.status === 'ok'){
-            salesArray = resultObj.data;
-            console.log(salesArray);
-        }
-    }).then( function(){
-        for(let i=0; i<salesArray.length; i++){
-        contentToAppend += `<div class="row">
+function appendSalesList(){
+    let currentPage = salesArray[displayPageNo];
+    contentToAppend = "";
+    for(let i=0; i<currentPage.length; i++){
+        contentToAppend += `<li class="list-group-item"><div class="row">
                                 <div class="row">
-                                    <div class="h5">${salesArray[i].title}</div>
+                                    <div class="h4"><a href="${DEAL_REDIRECT+currentPage[i].dealID}">${currentPage[i].title}</a></div>
                                 </div>
                                 <div class="row">
-                                    <div class="col-4"><img src="${salesArray[i].thumb}"></div>
+                                    <div class="col-4"><img alt="game-logo" src="${currentPage[i].thumb}"></div>
                                     <div class="col">
                                         <div class="row">
-                                            <div class="col-6">${salesArray[i].normalPrice}</div>
-                                            <div class="col-6">${salesArray[i].metacriticScore}</div>
+                                            <div class="col-6">Normal price: ${currentPage[i].normalPrice}</div>
+                                            <div class="col-6">Metacritic score: ${currentPage[i].metacriticScore}</div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-6">${salesArray[i].salePrice}</div>
-                                            <div class="col-6">${salesArray[i].steamRatingText}</div>
+                                            <div class="col-6">Sale price: ${currentPage[i].salePrice} (-${Math.floor(currentPage[i].savings)}%)</div>
+                                            <div class="col-6">Steam review: ${currentPage[i].steamRatingText}</div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>`;
+                            </div></li>`;
+    }
         $('#deal-show').html(contentToAppend);
-        }
+}
+
+
+$(document).ready(function(){
+    getJSONData(STEAM_DEALS + ON_SALE).then(
+        function(resultObj){
+            if(resultObj.status === 'ok'){
+                for(let i=0; i<resultObj.data.length; i+=10){
+                    salesArray.push(resultObj.data.slice(i, i+10));
+                }
+        }}).then( function(){
+            appendPagination();
+            appendSalesList()
     });
 });
